194febbc7d874342dd6ccc47a00f6522a973caf3
Hitansu123 <hitansupanda123@gmail.com>
2025-02-09 20:15:38 +0530

    Making user input handling better

--- main.go
+++ main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Enter your text")
 
-	var UserInput string
+	hash := make(map[string]string)
+
 	for {
-		fmt.Scan(&UserInput)
+		Input := bufio.NewReader(os.Stdin)
+		UserInput, _ := Input.ReadString('\n')
+		Splits := strings.Split(UserInput, " ")
 		if UserInput == "Ping" {
 
 			fmt.Println("Pong")
 		}
+		fmt.Println(Splits)
+		fmt.Println(hash)
 	}
 }
