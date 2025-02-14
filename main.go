4b602656b4b54298d0f03d65ef33df2bd4faeac3
Hitansu123 <hitansupanda123@gmail.com>
2025-02-14 23:32:53 +0530

    Added Delete Function

--- main.go
+++ main.go
@@ -12,19 +12,19 @@ func main() {
 	fmt.Println("Enter your text")
 
 	var hash sync.Map
-	//var wg sync.WaitGroup
+	var wg sync.WaitGroup
 
 	for {
 		Input := bufio.NewReader(os.Stdin)
 		UserInput, _ := Input.ReadString('\n')
 		newInput := strings.TrimSpace(UserInput)
 		Splits := strings.Split(newInput, " ")
-		if Splits[0] == "Ping" {
-
+		switch Splits[0] {
+		case "Ping":
 			fmt.Println("Pong")
-		} else if Splits[0] == "SET" {
+		case "SET":
 			setvalue(&hash, Splits)
-		} else if Splits[0] == "GET" {
+		case "GET":
 			key := Splits[1]
 			value, ok := hash.Load(key)
 			if ok {
@@ -32,7 +32,22 @@ func main() {
 			} else {
 				fmt.Println("Key does not exsist")
 			}
+		case "DEL":
+			wg.Add(1)
+			go deleteData(Splits[1], &hash, &wg)
+
 		}
+		wg.Wait()
+	}
+}
+
+func deleteData(key string, hash *sync.Map, wg *sync.WaitGroup) {
+	defer wg.Done()
+	_, found := hash.Load(key)
+	if found {
+		hash.Delete(key)
+	} else {
+		fmt.Println("Key does not exsist")
 	}
 }
 
